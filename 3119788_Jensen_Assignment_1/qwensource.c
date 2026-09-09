9b2880bb68023bed90bb5e8379420601fb49c695
B3rt577 <edbert106@gmail.com>
2026-09-08 21:42:52 -0500

    add prologue to sourcefile

--- 3119788_Jensen_Assignment_1/qwensource.c
+++ 3119788_Jensen_Assignment_1/qwensource.c
@@ -1,4 +1,17 @@
-// genAI qwen3:1.7b
+/*
+Author Name: Edbert Jensen
+Program Name: EECS 348 Assignment 1
+Brief description: C program that picks a secret number between 1 and 10. The user gets up to 3 tries to guess the number
+Inputs: 3 guesses from user
+Outputs: ask user to enter number from 1-10. tell user if guess was correct, high or low, and game over if all guesses were incorrect.
+Collaborators: qwen3:1.7b
+Other sources: 
+Creation date: 9/8/2026 7:21PM
+Revision date: 9/8/2026 9:39PM
+Revisions: add required prologue comments
+*/
+
+// genertated by qwen3:1.7b
 #include <stdio.h>
 
 int main() {
